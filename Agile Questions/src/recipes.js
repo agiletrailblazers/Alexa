3e79d950f36d1345eb7525d326ff86ba6c958c6f
Iftikharuddin Khan <Ifti@Iftikharuddins-MacBook-Pro.local>
2017-07-25 13:14:00 -0400

    Five questions are working.

--- Agile Questions/src/recipes.js
+++ Agile Questions/src/recipes.js
@@ -1,5 +1,9 @@
 module.exports = {
     "RECIPE_EN_US" : {
-        "agile": "A snow golem can be created by placing a pumpkin on top of  two snow blocks on the ground."
+        "agile": "Agile Software Development is an umbrella term for a set of methods and practices.  Solutions evolve through collaboration between self-organizing, cross-functional teams utilizing an incremental development strategy.",
+        "time Box": "A time box is a Fixed increment of time, rather than scope, for a given activity.",
+        "sprint": "A Sprint is a Time-boxed iteration of working time, usually 4 weeks.",
+        "sprint planning": "Sprint planning is a Meeting to negotiate the scope for next Sprint where the team pulls in work from the product Owner’s prioritized backlog.",
+        "sprint review": "A sprint review is Showcase of last Sprint’s work which can also accommodate team capacity, performance analysis and design reviews."
     }
 };
