1160ce92ee89f0e2cde639cddb98f366cced9d92
Iftikharuddin Khan <Ifti@Iftikharuddins-MacBook-Pro.local>
2017-07-25 14:07:10 -0400

    Working code with everything not referring to recipe.

--- Agile Questions/src/index.js
+++ Agile Questions/src/index.js
@@ -82,17 +82,17 @@ var languageStrings = {
     "en": {
         "translation": {
             "RECIPES": recipes.RECIPE_EN_US,
-            "SKILL_NAME": "Minecraft Helper",
-            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
+            "SKILL_NAME": "Agile Dictionary",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s agile? or What does agile mean? ... Now, what can I help you with.",
             "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
-            "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
-            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
-            "HELP_REPROMPT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            "DISPLAY_CARD_TITLE": "%s  - Description for %s.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s agile? or What does agile mean?, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMPT": "You can say things like, what\'s agile? or What does agile mean?, or you can say exit...Now, what can I help you with?",
             "STOP_MESSAGE": "Goodbye!",
             "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
             "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
-            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the description for %s. ",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that description. ",
             "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
         }
     }
