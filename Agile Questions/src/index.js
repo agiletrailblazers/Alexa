df343fca63f80b739b814f2a87a8c3740a3ad60e
Iftikharuddin Khan <Ifti@Iftikharuddins-MacBook-Pro.local>
2017-07-28 13:00:11 -0400

    change the way appId was specified to work properly.

--- Agile Questions/src/index.js
+++ Agile Questions/src/index.js
@@ -7,7 +7,7 @@ var digitalTransform = require('./digitaltransform')
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
+    alexa.appId = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
     alexa.registerHandlers(newSessionHandlers, categoryHandlers, glossaryHandlers, digitalHandlers);
